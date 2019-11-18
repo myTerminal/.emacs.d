99fe740ea8ed8403e5cf09cafa850275b474dda7
myTerminal <team.terminal@gmail.com>
2019-11-18 07:08:07 -0600

    borrow a function to toggle window-split direction from jonathanj

--- .emacs.d/myTerminal/lib.el
+++ .emacs.d/myTerminal/lib.el
@@ -63,6 +63,34 @@
                                  (message "File reloaded")))
         (t (message "You're not editing a file!"))))
 
+;; Credit: https://github.com/jonathanj
+(defun mt/window-toggle-split-direction ()
+  "Switches window split from horizontally to vertically, or vice versa."
+  (interactive)
+  (require 'windmove)
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window
+                                           neighbour-dir next-win)))))
+
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir)
+                                   other-buf))))))))
+
 (defun mt/remove-formatting (text)
   "Removes formatting of the supplied text."
   (interactive "sEnter text: ")
