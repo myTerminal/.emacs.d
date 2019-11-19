74361948b32471c0f0c379b4d1a09f90ebb2e049
myTerminal <team.terminal@gmail.com>
2019-11-19 11:03:13 -0600

    re-arrange functions

--- .emacs.d/myTerminal/lib.el
+++ .emacs.d/myTerminal/lib.el
@@ -17,6 +17,19 @@
   (global-set-key (kbd (car pair))
                   (cdr pair)))
 
+(defun mt/load-personalization-variables ()
+  "Loads personalization for current system"
+  (theme-looper-enable-theme mt/workstation-variables/color-theme)
+  (add-to-list 'default-frame-alist
+               `(font . ,mt/workstation-variables/font-default))
+  (add-hook 'prog-mode-hook
+            (lambda ()
+              (setq buffer-face-mode-face `(:family ,mt/workstation-variables/font-family-for-programming))
+              (buffer-face-mode)))
+  (set-frame-size (selected-frame)
+                  (car mt/workstation-variables/frame-dimensions)
+                  (cdr mt/workstation-variables/frame-dimensions)))
+
 (defun mt/set-zoning ()
   "Sets zoning timeout"
   (interactive)
@@ -56,6 +69,12 @@
   (prin1 (eval (read (current-kill 0)))
          (current-buffer)))
 
+(defun mt/remove-formatting (text)
+  "Removes formatting of the supplied text."
+  (interactive "sEnter text: ")
+  (kill-new text)
+  (message "Formatting removed, text copied to clipboard!"))
+
 (defun mt/reload-current-file ()
   "Reloads the file loaded in current buffer from the disk."
   (interactive)
@@ -90,22 +109,3 @@
                   (split-window-horizontally))
                 (set-window-buffer (windmove-find-other-window neighbour-dir)
                                    other-buf))))))))
-
-(defun mt/remove-formatting (text)
-  "Removes formatting of the supplied text."
-  (interactive "sEnter text: ")
-  (kill-new text)
-  (message "Formatting removed, text copied to clipboard!"))
-
-(defun mt/load-personalization-variables ()
-  "Loads personalization for current system"
-  (theme-looper-enable-theme mt/workstation-variables/color-theme)
-  (add-to-list 'default-frame-alist
-               `(font . ,mt/workstation-variables/font-default))
-  (add-hook 'prog-mode-hook
-            (lambda ()
-              (setq buffer-face-mode-face `(:family ,mt/workstation-variables/font-family-for-programming))
-              (buffer-face-mode)))
-  (set-frame-size (selected-frame)
-                  (car mt/workstation-variables/frame-dimensions)
-                  (cdr mt/workstation-variables/frame-dimensions)))
