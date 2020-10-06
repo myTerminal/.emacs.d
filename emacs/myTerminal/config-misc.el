d12d0241632be57280d5ac096ce42984724dbb05
myTerminal <team.terminal@gmail.com>
2020-10-06 00:43:51 -0500

    update MELPA URL

--- emacs/myTerminal/config-misc.el
+++ emacs/myTerminal/config-misc.el
@@ -78,7 +78,7 @@
 (setq package--init-file-ensured
       t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/")
+             '("melpa" . "https://melpa.org/packages/")
              t)
 (package-initialize)
 
