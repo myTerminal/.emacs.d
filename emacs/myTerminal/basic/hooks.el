69d97a1a721d659a7b92405179bd8037f02b0023
myTerminal <team.terminal@gmail.com>
2021-02-22 20:14:35 -0600

    split: hooks.el

---
+++ emacs/myTerminal/basic/hooks.el
@@ -0,0 +1,11 @@
+(add-hook 'after-init-hook
+          'global-company-mode)
+
+(add-hook 'prog-mode-hook
+          'display-line-numbers-mode)
+
+(add-hook 'prog-mode-hook
+          'outer-spaces-mode)
+
+(add-hook 'text-mode-hook
+          'flyspell-mode)
--- emacs/myTerminal/standard/hooks.el
+++ emacs/myTerminal/standard/hooks.el
@@ -1,15 +1,2 @@
-(add-hook 'after-init-hook
-          'global-company-mode)
-
-(add-hook 'prog-mode-hook
-          'display-line-numbers-mode)
-
 (add-hook 'prog-mode-hook
           'fira-code-mode)
-
-(add-hook 'prog-mode-hook
-          'outer-spaces-mode)
-
-(add-hook 'text-mode-hook
-          'flyspell-mode)
-
