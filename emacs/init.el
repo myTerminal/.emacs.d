8e435e733ad3e1f088b4a12c7548558862151e9c
myTerminal <team.terminal@gmail.com>
2021-07-14 14:42:46 -0500

    reduce private functions

--- emacs/init.el
+++ emacs/init.el
@@ -1,36 +1,34 @@
+(require 'cl-lib)
+
 ;; Initialize root
 (defvar mt/config-root
   (file-name-directory load-file-name))
 
-(defun mt/start-basic ()
-  "Loads basic configs"
-  (mt/load-file "basic/misc")
-  (mt/load-file "basic/variables")
-  (mt/load-file "basic/packages")
-  (mt/load-file "basic/file-associations")
-  (mt/load-file "basic/key-bindings")
-  (mt/load-file "basic/workstation")
-  (mt/load-file "basic/hooks"))
-
-(defun mt/start-standard ()
-  "Loads standard configs"
-  (mt/load-file "standard/misc")
-  (mt/load-file "standard/variables")
-  (mt/load-file "standard/packages")
-  (mt/load-file "standard/file-associations")
-  (mt/load-file "standard/key-bindings")
-  (mt/load-file "standard/workstation")
-  (mt/load-file "standard/hooks"))
-
 (defun mt/start ()
   "Loads core and then conditionally loads configs"
-  (load (expand-file-name "myTerminal/core"
-			              mt/config-root))
-  (mt/start-basic)
-  (if (display-graphic-p)
-      (mt/start-standard))
-  (mt/load-file "_zone-quotes-masseffect")
-  (mt/print-welcome-message))
+  (cl-flet* ((start-basic ()
+                          (mt/load-file "basic/misc")
+                          (mt/load-file "basic/variables")
+                          (mt/load-file "basic/packages")
+                          (mt/load-file "basic/file-associations")
+                          (mt/load-file "basic/key-bindings")
+                          (mt/load-file "basic/workstation")
+                          (mt/load-file "basic/hooks"))
+             (start-standard ()
+                             (mt/load-file "standard/misc")
+                             (mt/load-file "standard/variables")
+                             (mt/load-file "standard/packages")
+                             (mt/load-file "standard/file-associations")
+                             (mt/load-file "standard/key-bindings")
+                             (mt/load-file "standard/workstation")
+                             (mt/load-file "standard/hooks")))
+    (load (expand-file-name "myTerminal/core"
+			                mt/config-root))
+    (start-basic)
+    (if (display-graphic-p)
+        (start-standard))
+    (mt/load-file "_zone-quotes-masseffect")
+    (mt/print-welcome-message)))
 
 ;; Start
 (mt/start)
